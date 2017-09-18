7a04e8a22efae77feb196c790423683476a88e40
Matthieu Segret <matthieu.segret@gmail.com>
2017-09-18 16:37:29 +0200

    Simplification des tests

--- spec/requests/welcomes_spec.rb
+++ spec/requests/welcomes_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Welcomes', type: :request do
   describe 'GET /welcomes/hello' do
+    before { get '/welcomes/hello' }
+
     it 'returns status code 200' do
-      get '/welcomes/hello'
       expect(response).to have_http_status(200)
     end
 
     it 'returns message with hello' do
-      get '/welcomes/hello'
-      expect(JSON.parse(response.body)['message']).to eq('Hello world !')
+      expect(json[:message]).to eq('Hello world !')
     end
   end
 end
