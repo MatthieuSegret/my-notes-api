dd9487bd7f16089460303a5f25d96d1a3b6b1023
Matthieu Segret <matthieu.segret@gmail.com>
2017-09-18 18:03:01 +0200

    Création d'un modèle Note

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Note.destroy_all
+10.times do
+  Note.create(title: Faker::Book.title, content: Faker::Lorem.sentence)
+end
